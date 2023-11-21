fa6172812762a637a535301dc0968ef40177f2fa
Ben Kimani <benzoic09@gmail.com>
2023-11-21 13:15:51 -0800

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,27 +1,25 @@
-#include <stddef.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of int in ascending order
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
  *                        using Insertion Sort algorithm.
  * @list: Pointer to a pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = (*list)->next;
-	listint_t *temp = current->prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (current != NULL)
 	{
-
+		listint_t *temp = current->prev;
 		while (temp != NULL && temp->n > current->n)
 		{
+			/* Swap nodes */
 			if (temp->prev != NULL)
 				temp->prev->next = current;
-
 			if (current->next != NULL)
 				current->next->prev = temp;
 
@@ -33,7 +31,6 @@ void insertion_sort_list(listint_t **list)
 
 			if (current->prev == NULL)
 				*list = current;
-
 
 			/* Print the list after each swap */
 			print_list(*list);
